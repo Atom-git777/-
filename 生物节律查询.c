9582b942fcbea0a5209dc73241b79cc0c4897e52
Atom-git777 <61577182+Atom-git777@users.noreply.github.com>
2020-03-15 15:59:50 +0800

    生物节律查询

---
+++ 生物节律查询.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+int lastdays(int a,int b,int c){
+	int days=0;
+	int leap;
+	int x[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+	for(int i=0;i<b-1;i++){
+		days+=x[i];
+	}
+	if(a%4!=0) leap=0;
+	else if(a%100!=0) leap=1;
+	else if(a%400!=0) leap=0;
+	else leap=1;
+	if(leap&&b>=3) days=days+c+1;
+	else days=days+c;
+	return days;
+}
+
+int main(){
+	int start_year,start_month,start_day;
+	int fin_year,fin_month,fin_day;
+	int cutdays,adddays;
+	int totaldays=0,yeardays=0;
+	int leap;
+	printf("请输入出生年月日，格式为年  月  日\n");
+	scanf("%d%d%d",&start_year,&start_month,&start_day);
+	printf("请输入此时年月日，格式为年  月  日\n");
+	scanf("%d%d%d",&fin_year,&fin_month,&fin_day);
+    adddays=lastdays(fin_year,fin_month,fin_day);
+	cutdays=lastdays(start_year,start_month,start_day);
+	//printf("%d\n",cutdays);// for test  需要减去的天数 （出生年开始直到出生时）
+	//printf("%d\n",adddays);// for test   需要加上的天数 （此时年开始直到此时）
+	for(int i=start_year;i<fin_year;i++){
+	if(i%4!=0) leap=0;
+	else if(i%100!=0) leap=1;
+	else if(i%400!=0) leap=0;
+	else leap=1;
+	if(leap) yeardays+=366;
+	else yeardays+=365;
+	}
+	int m,n;
+	if(start_year%4==0&&start_year%100!=0||start_year%400==0)
+    {m=(fin_year-start_year-1)%4;
+    n=(fin_year-start_year-m-1)/4+2;}
+    else
+    {m=(fin_year-start_year-1)%4;
+    n=(fin_year-start_year-m-1)/4+1;}
+	totaldays=yeardays+adddays-cutdays;
+	printf("从出生到今日总天数：%d\n",totaldays);//for test （从出生到今日总共的天数）
+	printf("出生年到现在的闰年数：%d\n",n);
+    int a,b,c;
+	a=totaldays%23;
+	b=totaldays%28;
+	c=totaldays%33;
+	printf("您的体力节律为：%d\n",a);
+	printf("您的情绪节律为：%d\n",b);
+	printf("您的智力节律为：%d\n",c);
+	system("pause");
+}
